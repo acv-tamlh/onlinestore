895128f95ad1ff11c6fa038f045728373b1fdc82
TamLH <tamlh@atmarkcafe.org>
2018-03-28 21:18:48 +0700

    add data seed product, productgroup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328112311) do
+ActiveRecord::Schema.define(version: 20180328112903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180328112311) do
     t.string "image"
     t.string "refurl"
     t.string "description"
+    t.string "price"
     t.index ["productgroups_id"], name: "index_products_on_productgroups_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-  require 'amazon/ecs'
-
-  ACCESS_KEY_ID = "AKIAIXMVTFBCJZVBBALQ"
-
-  # Your Secret Key corresponding to the above ID, as taken from the Your Account page
-  SECRET_KEY = "MB7Y8Qj3BWub4o10f1VbiTH2t+1tiknBufRMfC8g"
-
-  # The region you are interested in
+  ACCESS_KEY_ID = "AKIAJWPWTVNYFLW7EKHQ"
+  SECRET_KEY = "C9ZIPiqI5j31xbH8N83rNOzp4XAQ8FuRlVUGlMTy"
   ENDPOINT = "webservices.amazon.in"
-
-  REQUEST_URI = "/onca/xml"
-
   ASSOCIATE_TAG = 'onlinestore'
 
 
@@ -26,40 +10,35 @@
       options[:AWS_secret_key] = SECRET_KEY
       options[:associate_tag] = ASSOCIATE_TAG
     end
-    # To replace default options
-    # Amazon::Ecs.options = { ... }
 
-    # To override default options
-    res = Amazon::Ecs.item_search('ruby', {:search_index => 'VideoGames', :response_group => 'Small', :sort => 'salesrank', :country => 'in'})
+Productgroup.delete_all
+Product.delete_all
 
-    res.is_valid_request?
-    res.has_error?
-    res.error                                 # error message
-    res.total_pages
-    res.total_results
-    res.item_page                             # current page no if :item_page option is provided
+productGroups = ['Book','DVD','Toys','VideoGames','All']
 
-    # Find elements matching 'Item' in response object
+productGroups.each do |productGroup|
+  keywords = ['game', 'apple', 'phone', 'asus', 'friends']
+  prg = Productgroup.create!(title: productGroup)
+  keywords.each do |kw|
+    res = Amazon::Ecs.item_search(kw, {
+                                  :search_index => productGroup,
+                                  :response_group => 'Medium',
+                                  :sort => 'salesrank'
+                                  })
     res.items.each do |item|
-      # Retrieve string value using XML path
+      item_attributes = item.get_element('ItemAttributes')
+      # puts item
       asin = item.get('ASIN')
       title = item.get('ItemAttributes/Title')
       artist = item.get('ItemAttributes/Artist')
-      # puts item.get('ItemAttributes/ProductGroup')
-      puts '======='
-      # puts item.get('ItemAttributes/Manufacturer')
-      # puts item.get('ItemAttributes/Creator')
-
-      # Return Amazon::Element instance
-      item_attributes = item.get_element('ItemAttributes')
-      # puts item_attributes.get('Title')
-      # puts item_attributes.get('MediumImage/URL')
-
-
-      # item_attributes.get_unescaped('Title') # unescape HTML entities
-      # item_attributes.get_array('Author')    # ['Author 1', 'Author 2', ...]
-
-      # Return a hash object with the element names as the keys
-      # item.get_hash('SmallImage/URL') # {:url => ..., :width => ..., :height => ...}
-      Product.create!(asin: asin, title: title, artist: artist)
+      image = item.get('MediumImage/URL')
+      price = item_attributes.get("ListPrice/FormattedPrice")
+      refurl = item.get('DetailPageURL')
+      description = item.get('EditorialReviews/EditorialReview/Content')
+      puts res.error                                 # error message
+      puts asin
+      Product.create!(asin: asin, title: title, artist: artist, price: price, productgroups_id: prg.id, image: image, refurl: refurl, description: description)
     end
+  end
+
+end
