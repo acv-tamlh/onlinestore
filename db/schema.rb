74dee85c1df8d7f199c6edafe375f23dd42088bc
TamLH <tamlh@atmarkcafe.org>
2018-03-28 18:24:31 +0700

    add product description migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327095826) do
+ActiveRecord::Schema.define(version: 20180328112311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "productgroups", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "asin"
+    t.string "title"
+    t.string "artist"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "productgroups_id"
+    t.string "image"
+    t.string "refurl"
+    t.string "description"
+    t.index ["productgroups_id"], name: "index_products_on_productgroups_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
