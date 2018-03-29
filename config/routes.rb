97a72ac198db8bd7b352dda146e7573970aaf647
TamLH <tamlh@atmarkcafe.org>
2018-03-29 17:40:09 +0700

    add controller cart, orderitem

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products
   resources :productgroups
+  resources :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
