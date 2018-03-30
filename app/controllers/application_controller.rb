8ca1ae04e4675d8db7bf84d440dedab01fd9c13a
TamLH <tamlh@atmarkcafe.org>
2018-03-30 09:10:28 +0700

    fix No route matches {:action=>"show", :controller=>"carts", :id=>nil}, missing required keys: [:id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_order
 
   def current_order
-    if !session[:order_id].nil?
-      Order.find(session[:order_id])
-    else
-      Order.new
-    end
-        # @order = Order.find(session[:order_id])
-        # rescue ActiveRecord::RecordNotFound
-        # @order = Order.create
-        # session[:order_id] = @order.id
-
-        # Order.find(session[:order_id])
-        # rescue ActiveRecord::RecordNotFound
-        # order = Order.create
-        # session[:order_id] = order.id
-        # order
+    Order.find(session[:order_id])
+    rescue ActiveRecord::RecordNotFound
+    order = Order.create
+    session[:order_id] = order.id
+    order
   end
 end
