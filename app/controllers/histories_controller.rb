338b10d3de2668533b7a80b9678471adbf91d3cf
TamLH <tamlh@atmarkcafe.org>
2018-03-31 16:56:38 +0700

    add history-page details

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,10 +1,10 @@
 class HistoriesController < ApplicationController
   before_action :authenticate_user!
- 
+
   def index
     @histories = current_user.orders
   end
   def show
-    @history = current_user.order.find(params[:order_id])
+    @history = current_user.orders.find(params[:id])
   end
 end
