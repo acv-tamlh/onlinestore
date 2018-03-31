b3e7cf2ff650a615ce54e597b57a873b3d3ccbc3
TamLH <tamlh@atmarkcafe.org>
2018-03-31 10:28:03 +0700

    update user info - deliver page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :order_items, only: [:create, :update, :destroy]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
