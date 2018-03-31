c698315ee636aa1577f2d2281805f2897a553d95
TamLH <tamlh@atmarkcafe.org>
2018-03-31 16:22:32 +0700

    fix Couldn't find Order with 'id'=

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def current_order
     Order.find(session[:order_id])
     rescue ActiveRecord::RecordNotFound
-    @order = Order.create
+    order = Order.create
     session[:order_id] = order.id
     order
   end
