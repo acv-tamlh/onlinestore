9116cdc7723148d91a6763ef8d77eac4955f97b2
tamlhacv <37397804+tamlhacv@users.noreply.github.com>
2018-04-02 17:34:02 +0700

    Revert "Add devise"

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# require 'awesome_print'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
