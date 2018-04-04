a03290b2c9a4756a12afc05b5cc23c95c11b7497
TamLH <tamlh@atmarkcafe.org>
2018-04-04 08:58:27 +0700

    rspec:controller order

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe OrdersController, type: :controller do
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, params: { id: create(:order).id }
+      expect(response).to have_http_status(:success)
+    end
+    it 'render template' do
+      get :show, params: { id: create(:order).id }
+      expect(response).to render_template :show
+    end
+  end
+end
