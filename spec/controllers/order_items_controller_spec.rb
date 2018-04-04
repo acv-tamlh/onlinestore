cee38fa32f9cd30eddbe37828ec1ce85ad4550bc
TamLH <tamlh@atmarkcafe.org>
2018-04-04 16:23:12 +0700

    rspec controller order_item

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe OrderItemsController, type: :controller do
-
+  let!(:product) { create(:product) }
   describe "GET #update" do
-    let!(:order_item) { create(:order_item, quantity: 100) }
-    # it "returns http success" do
-    #   old_quantity = order_item.quantity
-    #   put :update, params: {order_item: FactoryBot.attributes_for(:order_item, quantity: 1)  }
-    #   order_item.reload
-    #   new_quantity = order_item.quantity
-    #   expect(old_quantity).to eq new_quantity
-    # end
+    # let!(:order_item) { create(:order_item) }
+    def params(quantity = 123)
+      order_item_params = { quantity: quantity }
+    end
+    it "returns http success" do
+      user = User.create!(email: 'asdasdasdasdasd@email.com', password: '123213123')
+      order = Order.create!(user_id: user.id)
+      order_item = OrderItem.create(order_id: order.id, product_id: product.id, quantity: 12)
+      binding.pry
+      patch :update, params: {id: order_item.id, order_item: {quantity: 123}  }
+      order_item.reload
+      new_quantity = order_item.quantity
+      expect(old_quantity).to eq new_quantity
+    end
   end
 end
