8357eaf0065d90d3cd72528a76b48b668b1a32bc
TamLH <tamlh@atmarkcafe.org>
2018-04-05 10:16:39 +0700

    config capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
+require 'rspec/core'
+require 'capybara/dsl'
+require 'capybara/rspec/matchers'
+require 'capybara/rspec/features'
+require 'capybara/rspec/matcher_proxies'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
-
+require 'rspec/rails'
+require "capybara/rspec"
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
@@ -13,4 +19,22 @@ RSpec.configure do |config|
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
   config.include FactoryBot::Syntax::Methods
+
+
+  config.include Capybara::DSL, type: :feature
+  config.include Capybara::RSpecMatchers, type: :feature
+  config.include Capybara::RSpecMatchers, type: :view
+  config.after do
+    if self.class.include?(Capybara::DSL)
+      Capybara.reset_sessions!
+      Capybara.use_default_driver
+    end
+  end
+  config.before do
+    if self.class.include?(Capybara::DSL)
+      example = RSpec.current_example
+      Capybara.current_driver = Capybara.javascript_driver if example.metadata[:js]
+      Capybara.current_driver = example.metadata[:driver] if example.metadata[:driver]
+    end
+  end
 end
