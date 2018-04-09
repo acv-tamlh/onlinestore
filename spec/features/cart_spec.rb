e9a1a3e321b1b99f08c8606debeff87d62ea209a
TamLH <tamlh@atmarkcafe.org>
2018-04-09 11:41:58 +0700

    .

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -3,40 +3,31 @@ require 'rails_helper'
 RSpec.describe 'Relate To Cart', type: :feature do
   describe 'Add item to cart' do
     it 'add item sucessfull' do
-      
     end
     it 'add item failure' do
-      pending ('..')
     end
   end
   describe 'Delete item from cart' do
     it 'delete sucessfully' do
-      pending ('..')
     end
     it 'delete failure' do
-      pending ('..')
     end
   end
   describe 'Update item quantity' do
     it 'successfully' do
-      pending
     end
     it 'failure' do
-      pending
     end
   end
   describe 'Change status' do
     context 'In Proecss' do
-
     end
     context 'Waiting' do
       it 'user login ?' do
-        pending
       end
     end
     context 'Paid' do
       it "checkout ?" do
-        pending
       end
     end
   end
