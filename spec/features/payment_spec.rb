866a7c6bf315090dd05f39c5d4e421af2805ffd6
TamLH <tamlh@atmarkcafe.org>
2018-04-10 10:45:55 +0700

    paypal test -- failure

--- spec/features/payment_spec.rb
+++ spec/features/payment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+include Warden::Test::Helpers
 
-RSpec.describe 'Relate To Payment', type: :feature do
+RSpec.describe 'Relate To Payment', type: :feature, js: true do
+  let!(:product) { create(:product) }
+  let!(:user) { create(:user) }
+  describe 'Pay' do
+    def add_order_item_to_cart
+      visit root_path
+      click_on('Add to Cart')
+      click_on('1 Item in cart ($199.00)')
+    end
+    it 'without login' do
+      add_order_item_to_cart
+      expect(page).to have_content('Register to checkout')
+    end
+    it 'signed in' do
+      add_order_item_to_cart
+      login_as(user)
+      click_on('1 Item in cart ($199.00)')
+      expect(page).to have_content('Checkout Infomation')
+      expect(page).to have_button('Checkout')
+      first(:css, '.btn.btn-primary').click
+      # #error in paypal
+      # if user.full_name.nil?
+      #   expect(page).to have_content('Update infomation')
+      # else
+      #   expect(page).to have_content('Payment Method')
+      # end
 
+    end
+  end
 end
